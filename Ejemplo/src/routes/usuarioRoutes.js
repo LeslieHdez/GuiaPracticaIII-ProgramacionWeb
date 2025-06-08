3fff30a80dc3564deb17c54bafb15b83d50f4e35
LeslieHdez <lesliehernandz17@gmail.com>
2025-06-08 14:02:54 -0600

    Proyecto listo para Render

---
+++ Ejemplo/src/routes/usuarioRoutes.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+const usuarioController = require('../controllers/usuarioController');
+
+router.post('/registro', usuarioController.registraUsuario); //Ruta para registrar usuarios
+router.get('/', usuarioController.obtenerUsuarios); //Ruta para obtener los usuarios
+router.delete('/:id', usuarioController.eliminarUsuario); // Ruta para eliminar usuarios
+router.put('/:id', usuarioController.actualizarUsuario); // Rura para actualizar usuarios
+router.get('/:id', usuarioController.obetenerUsuarioPorId); // Ruta para obtener uuario por Id
+
+module.exports = router;
